63fd4d7b52d5d583ae719d29805651ae3c7b9951
Carlos Reyes <reyesrico@hotmail.com>
2021-06-10 22:45:42 -0700

    Call predictor per init not per predict.

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 
 white = ['http://localhost:8080','http://localhost:3000', 'http://localhost:5000']
 
+pred = Predictor()
+predInitialized = False
+
 @app.after_request
 def add_cors_headers(response):
     # r = request.referrer[:-1]
@@ -29,6 +32,29 @@ def hello():
 def teams():
   return { 'teams': getTeams() }
 
+@app.route('/init', methods=['GET'])
+def init():
+  pred.generateData()
+  pred.trainData()
+  predInitialized = True
+  return { "result": "initialized" }
+
+@app.route('/refresh', methods=['GET'])
+def refresh():
+  pred = Predictor()
+  pred.generateData()
+  pred.trainData()
+  predInitialized = True
+  return { "result": "refreshed" }
+
+@app.route('/test', methods=['GET'])
+def test():
+  pred = Predictor()
+  pred.generateData()
+  pred.trainData()
+  result = pred.predict(11, 3, 0, 0, 1, 1, 52)
+  return { "predict": result }
+
 # PREDICT
 # http://localhost:5000/predict?team_number=11&week_number=3&is_playoff=0&is_super_bowl=0&is_local=1&weather_wind=1&weather_humidity=52
 @app.route('/predict', methods=['GET'])
@@ -41,15 +67,13 @@ def predict():
   weatherWindMPH = request.args.get('weather_wind', '')
   weatherHumidity = request.args.get('weather_humidity', '')
 
-  pred = Predictor()
-  pred.generateData()
-  pred.trainData()
-  result = pred.predict(teamNumber, weekNumber, isPlayoff, isSuperBowl, is49ersLocal, weatherWindMPH, weatherHumidity)
-  print('Super result!!! => ' + str(result))
+  result = False
 
-  return {
-    "predict": result
-  }
+  if predInitialized:
+    result = pred.predict(teamNumber, weekNumber, isPlayoff, isSuperBowl, is49ersLocal, weatherWindMPH, weatherHumidity)
+    print('Super result!!! => ' + str(result))
+
+  return { "predict": result }
 
 if __name__ == '__main__':
     app.run()
